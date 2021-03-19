8bd134ec85ff1db30be14083803601d7af3be470
Guitao Liu <gtliu52@gmail.com>
2021-03-19 22:53:51 +0800

    chore: adjust left panel

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,14 @@ import {
   Text,
   Flex,
   Collapse,
+  Spacer,
   useDisclosure,
 } from '@chakra-ui/react'
 
 import { GoDashboard, GoSettings, GoServer } from 'react-icons/go'
 import { GiCarBattery } from 'react-icons/gi'
 import { BsFillLightningFill } from 'react-icons/bs'
+import { MdExpandLess, MdExpandMore } from 'react-icons/md'
 
 import FuelCell from './pages/FuelCell'
 import Home from './pages/Home'
@@ -27,15 +29,15 @@ const App = (): JSX.Element => {
   const { isOpen, onToggle, onClose } = useDisclosure()
   return (
     <Tabs variant="enclosed" isLazy orientation="vertical" h="100%">
-      <TabList py={3} bg="blue.400" w="60">
+      <TabList py={3} bg="blue.400" w="60" shadow="2xl">
         <Tab
           _selected={{ bg: 'white', color: 'black' }}
           onClick={onClose}
           color="white"
           my={2}
-          justifyContent="space-between"
+          justifyContent="flex-start"
         >
-          <Icon as={GoDashboard} boxSize={5} />
+          <Icon mr={2} as={GoDashboard} boxSize={5} />
           <Text fontSize="lg" fontWeight="semibold" letterSpacing="wide">
             Dashboard
           </Text>
@@ -45,12 +47,18 @@ const App = (): JSX.Element => {
           onClick={onToggle}
           color="white"
           my={2}
-          justifyContent="space-between"
+          justifyContent="flex-start"
         >
-          <Icon as={GoServer} boxSize={5} />
+          <Icon mr={2} as={GoServer} boxSize={5} />
           <Text fontSize="lg" fontWeight="semibold" letterSpacing="wide">
             Devices
           </Text>
+          <Spacer />
+          {isOpen ? (
+            <Icon as={MdExpandMore} boxSize={5} />
+          ) : (
+            <Icon as={MdExpandLess} boxSize={5} />
+          )}
         </Tab>
         <Collapse in={isOpen} animateOpacity>
           <Flex direction="column">
@@ -58,9 +66,9 @@ const App = (): JSX.Element => {
               _selected={{ bg: 'white', color: 'black' }}
               color="white"
               pl={8}
-              justifyContent="space-between"
+              justifyContent="flex-start"
             >
-              <Icon as={GiCarBattery} boxSize={4} />
+              <Icon mr={2} as={GiCarBattery} boxSize={4} />
               <Text fontSize="md" letterSpacing="wide">
                 Fuel Cell
               </Text>
@@ -69,9 +77,9 @@ const App = (): JSX.Element => {
               _selected={{ bg: 'white', color: 'black' }}
               color="white"
               pl={8}
-              justifyContent="space-between"
+              justifyContent="flex-start"
             >
-              <Icon as={BsFillLightningFill} boxSize={4} />
+              <Icon mr={2} as={BsFillLightningFill} boxSize={4} />
               <Text fontSize="md" letterSpacing="wide">
                 AC/DC
               </Text>
@@ -83,22 +91,26 @@ const App = (): JSX.Element => {
           onClick={onClose}
           color="white"
           my={2}
-          justifyContent="space-between"
+          justifyContent="flex-start"
         >
-          <Icon as={GoSettings} boxSize={5} />
+          <Icon mr={2} as={GoSettings} boxSize={5} />
           <Text fontSize="lg" fontWeight="semibold" letterSpacing="wide">
             Setting
           </Text>
         </Tab>
+        <Spacer />
+        <Tab color="white" isDisabled _hover={{ cursor: 'content-menu' }}>
+          © Xi'an Jiaotong University
+        </Tab>
       </TabList>
-      <TabPanels p={2} h="100%">
+      <TabPanels py={3} h="100%">
         <TabPanel>
           <Home />
         </TabPanel>
         <TabPanel h="100%">
           <Devices />
         </TabPanel>
-        <TabPanel>
+        <TabPanel h="100%">
           <FuelCell />
         </TabPanel>
         <TabPanel>
