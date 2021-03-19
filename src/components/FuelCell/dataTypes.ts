18978dd6521531799ef71e4bdf7fb420fe555ec2
Guitao Liu <gtliu52@gmail.com>
2021-03-19 22:56:06 +0800

    feat: finish fuel cell page

--- src/components/FuelCell/dataTypes.ts
+++ src/components/FuelCell/dataTypes.ts
@@ -1,65 +1,36 @@
-export type CellOutputVolt = number
-export type CellOutputCurrent = number
-export type CellOutputPower = number
-export type PowerStackMinVolt = number
-export type PowerStackMinNumber = number
-export type PowerStackMaxVolt = number
-export type PowerStackMaxNumber = number
-export type PressureHydrogen = number
-export type PressureCoolingWater = number
-export type PressureGas = number
-export type PressureMainHydrogenBottle = number
-export type PressureAttachedHydrogenBottle = number
-export type DCDCVolt = number
-export type DCDCCurrent = number
-export type DCDCPower = number
-export type LoadVolt = number
-export type LoadCurrent = number
-export type ConcentrationSystemRoom = number
-export type ConcentrationHydrogenRoom = number
-export type TemperatureGasIn = number
-export type TemperatureGasOut = number
-export type TemperatureCoolingWaterIn = number
-export type TemperatureCoolingWaterOut = number
-export type TemperatureSystemCabinet = number
-export type TemperatureDCDC = number
-export type SystemRuntimeHour = number
-export type SystemRuntimeMinute = number
-export type SystemRuntimeSecond = number
-
 export type FuelCellData =
-  | CellOutputVolt
-  | CellOutputCurrent
-  | CellOutputPower
-  | PowerStackMinVolt
-  | PowerStackMinNumber
-  | PowerStackMaxVolt
-  | PowerStackMaxNumber
-  | PressureHydrogen
-  | PressureCoolingWater
-  | PressureGas
-  | PressureMainHydrogenBottle
-  | PressureAttachedHydrogenBottle
-  | DCDCVolt
-  | DCDCCurrent
-  | DCDCPower
-  | LoadVolt
-  | LoadCurrent
-  | ConcentrationSystemRoom
-  | ConcentrationHydrogenRoom
-  | TemperatureGasIn
-  | TemperatureGasOut
-  | TemperatureCoolingWaterIn
-  | TemperatureCoolingWaterOut
-  | TemperatureSystemCabinet
-  | TemperatureDCDC
-  | SystemRuntimeHour
-  | SystemRuntimeMinute
-  | SystemRuntimeSecond
+  | 'CellOutputVolt'
+  | 'CellOutputCurrent'
+  | 'CellOutputPower'
+  | 'PowerStackMinVolt'
+  | 'PowerStackMinNumber'
+  | 'PowerStackMaxVolt'
+  | 'PowerStackMaxNumber'
+  | 'PressureHydrogen'
+  | 'PressureCoolingWater'
+  | 'PressureGas'
+  | 'PressureMainHydrogenBottle'
+  | 'PressureAttachedHydrogenBottle'
+  | 'DCDCVolt'
+  | 'DCDCCurrent'
+  | 'DCDCPower'
+  | 'LoadVolt'
+  | 'LoadCurrent'
+  | 'ConcentrationSystemRoom'
+  | 'ConcentrationHydrogenRoom'
+  | 'TemperatureGasIn'
+  | 'TemperatureGasOut'
+  | 'TemperatureCoolingWaterIn'
+  | 'TemperatureCoolingWaterOut'
+  | 'TemperatureSystemCabinet'
+  | 'TemperatureDCDC'
+  | 'SystemRuntimeHour'
+  | 'SystemRuntimeMinute'
+  | 'SystemRuntimeSecond'
 
 export interface DataRecord {
   id: number
   name: string
-  value: FuelCellData
+  value: number
   unit?: string
 }
--- src/pages/FuelCell.tsx
+++ src/pages/FuelCell.tsx
@@ -1,103 +1,46 @@
 import React, { useState } from 'react'
 
 import {
-  Grid,
-  GridItem,
   HStack,
   VStack,
-  Spacer,
-  Icon,
   Text,
-  Select,
-  Button,
+  Box,
+  useToast,
+  useMediaQuery,
 } from '@chakra-ui/react'
-import { MdSave, MdDelete } from 'react-icons/md'
 
-import DataTable, { DataTableProps } from '../components/FuelCell/DataTable'
+import DataTableGrid, {
+  useFuelCell,
+} from '../components/FuelCell/DataTableGrid'
+import Controller from '../components/FuelCell/Controller'
+import CANSetting from '../components/FuelCell/CANSetting'
 
 const FuelCell: React.FC = () => {
-  const [output, setOutput] = useState<DataTableProps>({
-    name: 'Output',
-    data: [
-      { id: 1, name: 'Cell Volt', value: 0, unit: 'V' },
-      { id: 2, name: 'Cell Current', value: 0, unit: 'A' },
-      { id: 3, name: 'Cell Power', value: 0, unit: 'W' },
-      { id: 4, name: 'DCDC Volt', value: 0, unit: 'V' },
-      { id: 5, name: 'DCDC Current', value: 0, unit: 'A' },
-      { id: 6, name: 'DCDC Power', value: 0, unit: 'W' },
-    ],
-  })
-  const [powerStack, setPowerStack] = useState<DataTableProps>({
-    name: 'Power Stack',
-    data: [
-      { id: 1, name: 'Min Volt', value: 0, unit: 'V' },
-      { id: 2, name: 'Min Number', value: 0 },
-      { id: 3, name: 'Max Volt', value: 0, unit: 'V' },
-      { id: 4, name: 'Max Number', value: 0 },
-    ],
-  })
-  const [pressure, setPressure] = useState<DataTableProps>({
-    name: 'Pressure',
-    data: [
-      { id: 1, name: 'Hydrogen', value: 0, unit: 'bar' },
-      {
-        id: 2,
-        name: 'Cooling Water',
-        value: 0,
-        unit: 'bar',
-      },
-      { id: 3, name: 'Gas', value: 0, unit: 'bar' },
-      {
-        id: 4,
-        name: 'Main Hydrogen Bottle',
-        value: 0,
-        unit: 'Mpa',
-      },
-      {
-        id: 5,
-        name: 'Attached Hydrogen Bottle',
-        value: 0,
-        unit: 'Mpa',
-      },
-    ],
-  })
-  const [load, setLoad] = useState<DataTableProps>({
-    name: 'Load',
-    data: [
-      { id: 1, name: 'Volt', value: 0, unit: 'V' },
-      { id: 2, name: 'Current', value: 0, unit: 'A' },
-    ],
-  })
-  const [concentration, setConcentration] = useState<DataTableProps>({
-    name: 'Concentration',
-    data: [
-      {
-        id: 1,
-        name: 'System Room',
-        value: 0,
-        unit: 'ppm',
-      },
-      {
-        id: 2,
-        name: 'Hydrogen Room',
-        value: 0,
-        unit: 'ppm',
-      },
-    ],
-  })
-  const [temperature, setTemperature] = useState<DataTableProps>({
-    name: 'Temperature',
-    data: [
-      { id: 1, name: 'Gas In', value: 0, unit: '℃' },
-      { id: 2, name: 'Gas Out', value: 0, unit: '℃' },
-      { id: 3, name: 'Cooling Water In', value: 0, unit: '℃' },
-      { id: 4, name: 'Cooling Water Out', value: 0, unit: '℃' },
-      { id: 5, name: 'System Cabinet', value: 0, unit: '℃' },
-    ],
-  })
+  const {
+    output,
+    setOutput,
+    powerStack,
+    setPowerStack,
+    load,
+    setLoad,
+    concentration,
+    setConcentration,
+    pressure,
+    setPressure,
+    temperature,
+    setTemperature,
+  } = useFuelCell(1)
+
+  const [upHour, setUpdateHour] = useState<number>(0)
+  const [upMinute, setUpdateMinute] = useState<number>(0)
+  const [upSecond, setUpdateSecond] = useState<number>(0)
 
   const [baudRate, setBaudRate] = useState<number>(500000)
-  const [isConnected, setIsConnected] = useState<boolean>(false)
+  const [canChannel, setCanChannel] = useState<number>(0)
+  const [isStart, setStart] = useState<boolean>(false)
+
+  const clearToast = useToast()
+  const [showCharts] = useMediaQuery('(min-width: 1536px)')
 
   const handleClear = () => {
     setOutput(() => {
@@ -148,79 +91,65 @@ const FuelCell: React.FC = () => {
       }))
       return updatedTemperature
     })
-    setIsConnected(false)
+    setUpdateSecond(0)
+    setUpdateMinute(0)
+    setUpdateHour(0)
+    setStart(false)
+    clearToast({
+      title: 'Done!',
+      description: 'Data is cleared and the connection is closed.',
+      status: 'success',
+      duration: 5000,
+      isClosable: true,
+    })
   }
 
   return (
     <VStack justifyContent="center">
-      <HStack>
-        <HStack w="100%" maxW="800px">
-          <Text fontSize="sm" w="25%">
-            CAN Baud Rate:
+      <HStack w="80%" justifyContent="center">
+        <VStack w="30%" justifyContent="center" alignItems="center">
+          <Text fontSize="3xl">UP TIME</Text>
+          <Text fontSize="3xl" fontWeight="bold" letterSpacing="wider">
+            {upHour < 10 ? `0${upHour}` : upHour}:
+            {upMinute < 10 ? `0${upMinute}` : upMinute}:
+            {upSecond < 10 ? `0${upSecond}` : upSecond}
           </Text>
-          <Select
-            w="35%"
-            fontSize="sm"
-            onChange={(event) => {
-              setBaudRate(Number(event.target.value) * 1000)
-            }}
-            defaultValue="500"
-          >
-            <option value="50">50 kBit/sec</option>
-            <option value="80">80 kBit/sec</option>
-            <option value="100">100 kBit/sec</option>
-            <option value="125">125 kBit/sec</option>
-            <option value="200">200 kBit/sec</option>
-            <option value="250">250 kBit/sec</option>
-            <option value="400">400 kBit/sec</option>
-            <option value="500">500 kBit/sec</option>
-            <option value="600">600 kBit/sec</option>
-            <option value="800">800 kBit/sec</option>
-            <option value="1000">1 MBit/sec</option>
-          </Select>
-          <Spacer />
-          <Button
-            leftIcon={<Icon as={MdSave} boxSize={5} />}
-            colorScheme="blue"
-          >
-            Save
-          </Button>
-          <Button
-            leftIcon={<Icon as={MdDelete} boxSize={5} />}
-            colorScheme="red"
-            variant="outline"
-            onClick={handleClear}
-          >
-            Clear
-          </Button>
-        </HStack>
+        </VStack>
+        <VStack w="40%" justifyContent="center">
+          <CANSetting setCanChannel={setCanChannel} setBaudRate={setBaudRate} />
+        </VStack>
+        <VStack w="30%" justifyContent="center">
+          <Controller
+            handleClear={handleClear}
+            isStart={isStart}
+            setStart={setStart}
+          />
+        </VStack>
       </HStack>
-      <Grid
-        h="60%"
-        maxW="800px"
-        templateRows="repeat(2, 1fr)"
-        templateColumns="repeat(3, 1fr)"
-        gap={1}
-      >
-        <GridItem>
-          <DataTable name={output.name} data={output.data} />
-        </GridItem>
-        <GridItem>
-          <DataTable name={powerStack.name} data={powerStack.data} />
-        </GridItem>
-        <GridItem>
-          <DataTable name={load.name} data={load.data} />
-        </GridItem>
-        <GridItem>
-          <DataTable name={concentration.name} data={concentration.data} />
-        </GridItem>
-        <GridItem>
-          <DataTable name={pressure.name} data={pressure.data} />
-        </GridItem>
-        <GridItem>
-          <DataTable name={temperature.name} data={temperature.data} />
-        </GridItem>
-      </Grid>
+
+      <HStack w="80%" justifyContent="center" spacing={2}>
+        <DataTableGrid
+          output={output}
+          powerStack={powerStack}
+          load={load}
+          concentration={concentration}
+          pressure={pressure}
+          temperature={temperature}
+        />
+        {showCharts && (
+          <Box
+            w="50%"
+            bg="cyan.200"
+            h="48"
+            m={4}
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Text color="white">Chart</Text>
+          </Box>
+        )}
+      </HStack>
     </VStack>
   )
 }
