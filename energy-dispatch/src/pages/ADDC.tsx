1a60d1ce8e1a7ee3f1516b1ee5555756387586f1
Guitao Liu <gtliu52@gmail.com>
2021-03-31 20:22:57 +0800

    chore: remove redundant

--- energy-dispatch/src/pages/ADDC.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const ADDC = (): JSX.Element => {
-  return <div>ACDC</div>
-}
-
-export default ADDC
--- energy-dispatch/src/pages/Setting.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-const Setting = (): JSX.Element => <div>Setting</div>
-
-export default Setting
