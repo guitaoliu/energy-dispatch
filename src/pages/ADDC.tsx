e03bcdb0541944cd4c4d34d598530a1c3269c82b
Guitao Liu <gtliu52@gmail.com>
2021-04-22 10:52:38 +0800

    chore: adjust placeholder for AC/DC transformer

--- src/pages/ADDC.tsx
+++ src/pages/ADDC.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Flex, Text } from '@chakra-ui/react'
 
 const ACDC: React.FC = () => {
-  return <div>addc</div>
+  return (
+    <Flex justifyContent="center" alignItems="center" h="full">
+      <Text fontSize="lg" fontWeight="semibold" letterSpacing="wide">
+        AC/DC Transformer
+      </Text>
+    </Flex>
+  )
 }
 
 export default ACDC
