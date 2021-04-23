04b7d24c9713d5a3c3d06bbe0f45c95a98cb2167
Guitao Liu <gtliu52@gmail.com>
2021-04-23 20:46:26 +0800

    chore: add doc strings

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,3 +1,6 @@
 import ElectronStore from 'electron-store'
 
+/**
+ * initialize ElectronStore for render process
+ */
 export default new ElectronStore()
--- src/utils/timeToString.ts
+++ src/utils/timeToString.ts
@@ -1,1 +1,5 @@
+/**
+ * convert time to two digits string
+ * @param num time number
+ */
 export default (num: number): string => (num < 10 ? `0${num}` : num.toString())
